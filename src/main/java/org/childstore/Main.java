ec86b666c48f3a34432536543c12c679dd7a26bb
Anyasdd <aiachysai63@gmail.com>
2025-04-24 02:41:37 +0600

    авторизация

--- src/main/java/org/childstore/Main.java
+++ src/main/java/org/childstore/Main.java
@@ -1,10 +1,18 @@
 package org.childstore;
 
-import org.childstore.ui.WorkerMenu;
+import org.childstore.model.User;
+import org.childstore.service.AuthService;
 
 public class Main {
     public static void main(String[] args) {
-        new WorkerMenu().show();
+        AuthService authService = new AuthService();
+        User user = authService.login();
+
+        if (user != null) {
+            user.showMenu();
+        } else {
+            System.out.println("Завершение программы.");
+        }
     }
 }
 
---
+++ src/main/java/org/childstore/service/AuthService.java
@@ -0,0 +1,30 @@
+package org.childstore.service;
+
+import org.childstore.model.*;
+
+import java.util.Scanner;
+
+public class AuthService {
+    private final Scanner scanner = new Scanner(System.in);
+
+    public User login() {
+        System.out.println("\nАвторизация");
+
+        System.out.print("Логин: ");
+        String username = scanner.nextLine();
+
+        System.out.print("Пароль: ");
+        String password = scanner.nextLine();
+
+        if (username.equals("worker") && password.equals("123")) {
+            return new Worker(username);
+        } else if (username.equals("director") && password.equals("123")) {
+            return new Director(username);
+        } else if (username.equals("delivery") && password.equals("123")) {
+            return new Deliveryman(username);
+        } else {
+            System.out.println("Неверные данные. Доступ запрещён.");
+            return null;
+        }
+    }
+}
