531fca40a8059fc83b779ee33c7d50160792b9fc
Anyasdd <aiachysai63@gmail.com>
2025-04-24 01:41:19 +0600

    обавлен ProductService-работы с товарами

---
+++ src/main/java/org/childstore/service/ProductService.java
@@ -0,0 +1,18 @@
+ppackage org.childstore.service;
+
+import org.childstore.model.Product;
+import org.childstore.repository.ProductRepository;
+
+import java.util.List;
+
+public class ProductService {
+    private final ProductRepository repository = new ProductRepository();
+
+    public void addProduct(Product product) {
+        repository.save(product);
+    }
+
+    public List<Product> getAllProducts() {
+        return repository.findAll();
+    }
+}
