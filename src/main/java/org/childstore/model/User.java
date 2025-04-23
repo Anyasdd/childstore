bed23b469e864d205b30035f10011e8b660753cf
Anyasdd <aiachysai63@gmail.com>
2025-04-24 02:17:13 +0600

    добавлен калсс юзер для ооп

---
+++ src/main/java/org/childstore/model/User.java
@@ -0,0 +1,15 @@
+package org.childstore.model;
+
+public abstract class User {
+    protected String username;
+
+    public User(String username) {
+        this.username = username;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public abstract void showMenu();
+}
